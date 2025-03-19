db120a0168b94a3a768241373c00727076cea023
Arnab Gupta <arnabgupta1111@gmail.com>
2025-03-19 21:02:42 +0600

    Implement blog controllers,services and routes

--- src/app/modules/message/message.interface.ts
+++
@@ -1,6 +0,0 @@
-export type TMessage = {
-  name: string;
-  phone: string;
-  email: string;
-  message: string;
-};
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,23 +1,23 @@
 import { Router } from 'express';
 import { BlogRoutes } from '../modules/blog/blog.routes';
-import { ProjectRouters } from '../modules/project/project.routes';
-import { MessageRoutes } from '../modules/message/message.routes';
+// import { ProjectRouters } from '../modules/project/project.routes';
+// import { MessageRoutes } from '../modules/message/message.routes';
 
 const router = Router();
 
 const moduleRoutes = [
-  {
-    path: '/message',
-    route: MessageRoutes,
-  },
+  // {
+  //   path: '/message',
+  //   route: MessageRoutes,
+  // },
   {
     path: '/blogs',
     route: BlogRoutes,
   },
-  {
-    path: '/projects',
-    route: ProjectRouters,
-  },
+  // {
+  //   path: '/projects',
+  //   route: ProjectRouters,
+  // },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
