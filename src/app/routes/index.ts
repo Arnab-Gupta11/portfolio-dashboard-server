526e96d2af6ee4598995710b48f5e274c91f962e
Arnab Gupta <arnabgupta1111@gmail.com>
2025-03-20 11:10:21 +0600

    Implement message controller,services and routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express';
 import { BlogRoutes } from '../modules/blog/blog.routes';
 import { ProjectRouters } from '../modules/project/project.routes';
-// import { MessageRoutes } from '../modules/message/message.routes';
+import { MessageRoutes } from '../modules/message/message.routes';
 
 const router = Router();
 
 const moduleRoutes = [
-  // {
-  //   path: '/message',
-  //   route: MessageRoutes,
-  // },
+  {
+    path: '/message',
+    route: MessageRoutes,
+  },
   {
     path: '/blogs',
     route: BlogRoutes,
